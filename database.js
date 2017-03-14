6afb1858a50efec2a01340cf4e41daa3101ba0f6
Terkel Gjervig <terkel@activetheory.net>
2017-03-13 21:24:21 -0700

    initial version

---
+++ database.js
@@ -0,0 +1,52 @@
+const MongoClient = require('mongodb').MongoClient
+
+const URI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds145299.mlab.com:45299/judgedbycover`
+
+let _db
+let _current
+
+module.exports.connectDB = function connectDB() {
+  return new Promise((resolve, reject) => {
+     MongoClient.connect(URI, function(err, db) {
+      if (err) reject(err)
+      _db = db
+      resolve(db)
+    })
+  })
+}
+
+module.exports.closeDB = function closeDB() {
+  _db.close((err) => {
+    if(err) return Promise.reject()
+    return Promise.resolve()
+  })
+}
+
+module.exports.getNextBook = function getNextBook() {
+  return new Promise((resolve, reject) => {
+    if (!_db) reject('No database provided')
+
+    let booksDB = _db.collection('books')
+    let progressDB = _db.collection('progress')
+
+    progressDB.findOne((err, item) => {
+      if (err) reject(err)
+      _current = item.current
+      booksDB.findOne({"index": _current}, (err, book) => {
+        if (err) reject(err)
+        resolve(book)
+      })
+    })
+  })
+}
+
+module.exports.setProgress = function progress() {
+  return new Promise((resolve, reject) => {
+    if (!_db) reject('No database connection open')
+    let progressDB = _db.collection('progress')
+    progressDB.updateOne({}, {$set: {current: _current+1}}, (err, progress) => {
+      if (err) reject('Could not update progress', err)
+      resolve(_current+1)
+    })
+  })
+}
---
+++ twitter.js
@@ -0,0 +1,34 @@
+const Twit = require('twit')
+
+var T = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  timeout_ms: 60*1000 // optional HTTP request timeout to apply to all requests. 
+})
+
+module.exports = function postTweet(status, media) {
+  return new Promise((resolve, reject) => {
+
+    // Upload data first
+    T.post('media/upload', { media_data: media }, function (err, data, response) {
+      var mediaIdStr = data.media_id_string
+      var altText = status
+      var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+
+      // Create metadata
+      T.post('media/metadata/create', meta_params, function (err, data, response) {
+        if (err) reject(err)
+        var params = { status: status, media_ids: [mediaIdStr] }
+
+        // Post
+        T.post('statuses/update', params, function (err, data, response) {
+          if (err) reject(err)
+          resolve(response)
+        })
+      })
+    })
+
+  })
+}
